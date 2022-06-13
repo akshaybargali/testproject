8e9f0c522b6062694a11124154f2931078a776b7
Akshay.Bargali <Akshay.Bargali@5414AKSHEY2571L.mind.motherson.com>
2022-06-13 09:53:01 +0530

    my first project

---
+++ src/test/java/com/helloMaven/project/AppTest.java
@@ -0,0 +1,11 @@
+package com.helloMaven.project;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void test() {
+        Assert.assertEquals("Hello Maven", new String("Hello Maven"));
+    }
+}
